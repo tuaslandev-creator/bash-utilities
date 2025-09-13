871f5eea095819f129faad58cff850653c8060d3
tuaslandev-creator <tuaslan.dev@gmail.com>
2025-09-13 18:47:50 +0300

    Update calculator.sh
    Added instructive comments

--- calculator.sh
+++ calculator.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+#
+#Simple command-line calculator.
+#
+#Prompts the user for the type of calculation and two numbers.
+#
+#Then outputs result.
+#
 
+#Asks and reads the type of calculation.
 echo "Welcome to calculator, please select the type of calculation you want to do"
 
 echo "1 - Addition"
@@ -9,14 +17,20 @@ echo "4 - Division"
 
 read type
 
+
+#Asks and reads the first number.
 echo "Very well, and our first number is:"
 
 read firstnumber
 
+
+#Asks and reads the second number.
 echo "And second is:"
 
 read secondnumber
 
+
+#Performs the calculation with the collected data.
 case "$type" in
     1)result=$(("$firstnumber" + "$secondnumber"))
     echo "Result is: $result";;
@@ -28,3 +42,4 @@ case "$type" in
     echo "Result is: $result";;
     *) echo "Opps... Something went wrong"
 esac
+vuaaaaaaa@vuaaaaaaa-E502SA:~/Desktop/Improve?May
