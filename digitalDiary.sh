1b238c0d76b534aac78b05b2a0cfdea1766cd0a5
tuaslandev-creator <tuaslan.dev@gmail.com>
2025-09-13 18:56:03 +0300

    Update digitalDiary.sh
    Added instructive comments.

--- digitalDiary.sh
+++ digitalDiary.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+#
+#Promts user for an optional file name.
+#
+#Promts user for a diary entry and appends it to the file.
+#
 
+
+#Sets the date format.
 date=$(date +%Y_%m_%d)
 
+
+#Asks for the file title.
 echo "What should the text called? (optional - pass empty)"
 read name
 
+
+#Asks for the diary entry.
 echo "Please enter your entry"
 read note
 
+
+#Appends the text to the file and created the file if it does not exist.
 echo "$note" >>  /usr/bin/digital_diary/"${date}_${name:-entry}.txt" 
